5f6ad6a78357a287015bf536749b9710ed8bb528
You <you@example.cm>
2024-10-19 03:24:49 -0700

    auth server rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
-"use client"
-import { useUser } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+// "use client"
+// import { useUser } from "@clerk/nextjs";
+ import { redirect } from "next/navigation";
+ import { auth } from '@clerk/nextjs/server'
+
+// const Welcome = () => {
+//   const { isLoaded, isSignedIn, user } = useUser();
+// console.log(user)
+//   console.log(isSignedIn)
+//    if(!isSignedIn){
+// redirect("./sign-up")
+//  }else{
+//     redirect("./wlcm")
+
+//  }
+
+
+// };
+
+// export default Welcome;
 
 const Welcome = () => {
-  const { isLoaded, isSignedIn, user } = useUser();
-console.log(user)
-  console.log(isSignedIn)
-   if(!isSignedIn){
+  // const { isLoaded, isSignedIn, user } = useUser();
+  const { userId }: { userId: string | null } = auth()
+
+   if(!userId){
 redirect("./sign-up")
  }else{
     redirect("./wlcm")
@@ -17,4 +34,3 @@ redirect("./sign-up")
 };
 
 export default Welcome;
-
