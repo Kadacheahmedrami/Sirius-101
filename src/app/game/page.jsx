c1760e5301cfaf8481afefe1cfd09a5d09f9bd65
You <you@example.cm>
2024-10-20 01:07:55 -0700

    rerun

--- src/app/game/page.jsx
+++ src/app/game/page.jsx
@@ -6,29 +6,36 @@ import QuizItem from "../../components/QuizItem"
 export default function Home() {
 
 const number = 1 
-const right = true
+const isright = true
   return (
 <>  
               =-
       <NavBar/>
   
 
-    <div className="border  ">
-<h1 className="text-2xl font-bold text-center   ">The Game </h1>
-<div className="  flex flex-col items-center   lg:flex-row ">
+    <div className="border w-full   ">
+<h1 className="text-3xl font-bold text-center   p-4 ">The Game </h1>
+<div className="  flex flex-col items-cente justify-center    lg:flex-row ">
   <div className="flex flex-col border px-8 ">
-    <h1 className="font-bold text-lg mb-4 w-[450px]">{number} Lorem, ipsigendi quid gfdgdfgfd fgsfdbdgfb bfgfdb em praesentiui hic.</h1>
+    <h1 className="font-bold text-lg mb-4 w-[450px]">{number +"-  " } Lorem, ipsigendi quid gfdgdfgfd fgsfdbdgfb bfgfdb em praesentiui hic.</h1>
 <QuizItem isright={true} alphabit={3}/>
 <QuizItem isright={false} alphabit={"A"}/>
 <QuizItem isright={false} alphabit={"B"}/>
 
-<QuizItem alphabit={'C'}/>
-
+<QuizItem alphabit={'C'} />
+<div className="flex flex-col mt-8 lg:flex-row justify-between ">
+<button className={"  p-2   flex items-center  border rounded-full"}  >
+ <h1 className="font-bold"> Cancel </h1>
+  </button>
+  <button className={` p-2   flex items-center  justify-center rounded-full   ${isright ? "bg-green-400 border-green-600 ": "bg-red-400 border-red-600"}`}>
+ <h1> Quit </h1>
+  </button>
+</div>
     <div>
 
     </div>
   </div>
-  <Image className="cursor-pointer relative left-16" src={'/robot.svg'} width={500} height={500} draggable="false" alt="Robot" />
+  <Image className="cursor-pointer relative left-16" src={'/robot.svg'} width={700} height={700} draggable="false" alt="Robot" />
 </div>
     </div>
      
--- src/components/QuizItem.jsx
+++ src/components/QuizItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function QuizItem({isright="",alphabit}) {
+function QuizItem({isright="",isselcted="",alphabit}) {
   return (
-    <div className={` p-2  mt-2 border  flex items-center  flex-row rounded-full   ${isright ? "bg-green-400": "bg-red-400 "}`}> 
+    <div className={` p-2  mt-2 border  flex items-center  flex-row rounded-full   ${isright ? "bg-green-400 border-green-600 ": "bg-red-400 border-red-600"}`}> 
  <p className='font-bold mr-2'>  {alphabit} </p>
-    <div className={`w-4 h-4 border   rounded-full  ${isright ? "bg-green-400": "bg-red-500 "}`}></div>
+    <div className={`w-4 h-4 border bg-transparent  rounded-full  ${isright ? "bg-green-400": "bg-red-500 "}`}></div>
   <h1 className='ml-4 w-[400px]'>
  Lorem ipsum dolor sit amet consecrferendis.
 </h1>
